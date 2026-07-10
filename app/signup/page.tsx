19608af2f92f43b129197d3ca8155ffc29ce90c3
n47h4n7357 <nsearles+n47h4n7357@gmail.com>
2026-07-10 11:58:34 -0700

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,6 +4,7 @@ export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
+        testing 7/10 11:58am
         <SignupForm />
       </div>
     </div>
